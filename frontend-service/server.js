dfece28e9c78b1ae526215957e77afe5e48f5aa2
Wisdom Shaibu <shaibuwisdom@gmail.com>
2025-08-02 13:56:59 +0000

    server

--- frontend-service/server.js
+++ frontend-service/server.js
@@ -9,11 +9,11 @@ const PORT = process.env.PORT || 3007;
 app.use(express.json());
 app.set('view engine', 'ejs');
 
-app.get('/', async (req, res) => {
+app.get('/frontend', (req, res) => {
   res.send(`<h1>Welcome to Wizfi Microservices UI</h1>`);
 });
 
-app.get('/dashboard', async (req, res) => {
+app.get('/frontend/dashboard', async (req, res) => {
   try {
     const jwt = req.headers.authorization;
 
@@ -27,7 +27,7 @@ app.get('/dashboard', async (req, res) => {
   }
 });
 
-app.get('/health', (req, res) => res.send('Frontend is healthy'));
+app.get('/frontend/health', (req, res) => res.send('Frontend is healthy'));
 
 app.listen(PORT, () => {
   console.log(`Frontend running on port ${PORT}`);
